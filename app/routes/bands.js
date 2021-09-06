2ecc7348f966df99e406bf7bc30d1c9568e461f6
Matthew Donnelly <brilliantrocketscientist@outlook.com>
2021-09-05 17:10:10 -0700

    Chapter 5

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -4,15 +4,25 @@ import { tracked } from '@glimmer/tracking';
 class Band {
   @tracked name;
 
-  constructor(name) {
+  constructor({ id, name, songs }) {
+    this.id = id;
     this.name = name;
+    this.songs = songs.map((songJSON) => new Song(songJSON));
+  }
+}
+
+class Song {
+  constructor({ title, rating, band }) {
+    this.title = title;
+    this.rating = rating ?? 0;
+    this.band = band;
   }
 }
 
 export default class BandsRoute extends Route {
   async model() {
-    const response = await fetch('http://localhost:5000/v1/test');
+    const response = await fetch('http://localhost:5000/v1/bands');
     const json = await response.json();
-    return json.data.map((bandJSON) => new Band(bandJSON.name));
+    return json.bands.map((bandJSON) => new Band(bandJSON));
   }
 }
--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,7 @@ module.exports = function (environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+      SERVER_HOST: 'http://localhost:5000/v1/'
     },
   };
 
