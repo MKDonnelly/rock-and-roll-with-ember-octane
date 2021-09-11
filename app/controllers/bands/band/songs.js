35af22254cc907358cb73f140a81170c6203468a
Matthew Donnelly <brilliantrocketscientist@outlook.com>
2021-09-11 10:45:02 -0700

    API fixes

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import Song from 'rock-and-roll-ember-octane/models/band';
+import Song from 'rock-and-roll-ember-octane/models/song';
 
 export default class BandsBandSongsController extends Controller {
   @tracked showAddSong = true;
